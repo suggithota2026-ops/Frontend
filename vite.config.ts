f67ad7264e123be55e480cd76f8386ab71a7e1cc
Shrawani kale <kaleshrawani16@gmail.com>
2026-01-12 11:16:33 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,12 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
       },
       "/uploads": {
-        target: "http://localhost:3000",
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
       },
